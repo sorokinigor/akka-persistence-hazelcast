92545d69d12e6dbb285751af952b955dfa4ce879
Igor Sorokin <igorsorokin808@gmail.com>
2017-01-13 20:16:09 +0300

    #2: Adding comments to 'reference.conf'

--- src/test/scala/akka/persistence/hazelcast/journal/MapJournalSpecs.scala
+++ src/test/scala/akka/persistence/hazelcast/journal/MapJournalSpecs.scala
@@ -7,12 +7,18 @@ import com.typesafe.config.ConfigFactory
 /**
   * @author Igor Sorokin
   */
-class MapJournalWithDisabledTransactionsSpec extends JournalSpec(ConfigFactory.load("application-transaction-disabled.conf")) {
+class MapJournalWithDisabledTransactionsSpec
+  extends JournalSpec(ConfigFactory.load("application-transaction-disabled.conf")) {
+
   override protected def supportsRejectingNonSerializableObjects: CapabilityFlag = CapabilityFlag.on()
 
   override def supportsAtomicPersistAllOfSeveralEvents: Boolean = false
+
 }
 
-class MapJournalWithEnabledTransactionsSpec extends JournalSpec(ConfigFactory.load("application-transaction-enabled.conf")) {
+class MapJournalWithEnabledTransactionsSpec
+  extends JournalSpec(ConfigFactory.load("application-transaction-enabled.conf")) {
+
   override protected def supportsRejectingNonSerializableObjects: CapabilityFlag = CapabilityFlag.on()
+
 }
