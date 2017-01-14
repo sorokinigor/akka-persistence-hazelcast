1eef181c797b9ee7470342ab6301b9647f25a3c4
Igor Sorokin <igorsorokin808@gmail.com>
2017-01-15 00:31:11 +0300

    #2: 'persistAll' - serialization and 'fail-on-non-atomic-persist-all' flag tests

--- src/test/scala/akka/persistence/hazelcast/journal/MapJournalSpecs.scala
+++ src/test/scala/akka/persistence/hazelcast/journal/MapJournalSpecs.scala
@@ -1,7 +1,10 @@
 package akka.persistence.hazelcast.journal
 
-import akka.persistence.CapabilityFlag
+import akka.actor.Actor
+import akka.persistence.JournalProtocol._
 import akka.persistence.journal.JournalSpec
+import akka.persistence.{AtomicWrite, CapabilityFlag, PersistentRepr}
+import akka.testkit.{EventFilter, TestProbe}
 import com.typesafe.config.ConfigFactory
 
 /**
@@ -13,18 +16,64 @@ class MapJournalWithDisabledTransactionsSpec extends JournalSpec(ConfigFactory.l
 
   override def supportsAtomicPersistAllOfSeveralEvents: Boolean = false
 
+  "journal" must {
+    "fail on 'persistAll'" in {
+      val probe = TestProbe()
+      val events = 1L to 5L map(sequenceNr => PersistentRepr(
+        payload = s"a-$sequenceNr", sequenceNr = sequenceNr, persistenceId = pid, sender = probe.ref,
+        writerUuid = writerUuid))
+
+      journal ! WriteMessages(List(AtomicWrite(events)), probe.ref, actorInstanceId)
+      probe.expectMsgPF() {
+        case WriteMessagesFailed(exception) =>
+          exception shouldBe an [UnsupportedOperationException]
+      }
+    }
+  }
+
 }
 
 class MapJournalWithEnabledTransactionsSpec
-  extends JournalSpec(ConfigFactory.load("application-transaction-enabled.conf")) {
+  extends JournalSpec(ConfigFactory.load("application-transaction-enabled.conf"))
+  with RejectingNonSerializableForPersistAllSpec {
 
   override protected def supportsRejectingNonSerializableObjects: CapabilityFlag = CapabilityFlag.on()
 
 }
 
 class MapJournalWithNonAtomicPersistAllSpec
-  extends JournalSpec(ConfigFactory.load("application-non-atomic-persist-all.conf")) {
+  extends JournalSpec(ConfigFactory.load("application-non-atomic-persist-all.conf"))
+  with RejectingNonSerializableForPersistAllSpec {
 
   override protected def supportsRejectingNonSerializableObjects: CapabilityFlag = CapabilityFlag.on()
+
+}
+
+trait RejectingNonSerializableForPersistAllSpec {
+  this: JournalSpec =>
+
+  "journal" must {
+    "reject non-serializable events" in EventFilter[java.io.NotSerializableException]().intercept {
+      val payload = new Object()
+      val sequenceNumberRange = 1L to 5L
+      val notSerializableEvents = sequenceNumberRange
+        .map(sequenceNr => PersistentRepr(payload, sequenceNr = sequenceNr, persistenceId = pid,
+          sender = Actor.noSender, writerUuid = writerUuid))
+
+      val probe = TestProbe()
+      journal ! WriteMessages(List(AtomicWrite(notSerializableEvents)), probe.ref, actorInstanceId)
+
+      probe.expectMsg(WriteMessagesSuccessful)
+      sequenceNumberRange.foreach(sequenceNumber => {
+        probe.expectMsgPF() {
+          case rejectedMessage: WriteMessageRejected =>
+            val rejected = rejectedMessage.message
+            rejected.payload.asInstanceOf[AnyRef] should be theSameInstanceAs payload
+            rejected.sequenceNr shouldBe sequenceNumber
+            rejected.persistenceId shouldBe pid
+        }
+      })
+    }
+  }
 
 }
