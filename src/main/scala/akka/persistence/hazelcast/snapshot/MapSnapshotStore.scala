c9daa0cf1c0f5c47594fcec7a896fbf27e26027b
Igor Sorokin <igorsorokin808@gmail.com>
2017-01-15 04:39:25 +0300

    #3: Adding logging to MapSnapshotStore

--- src/main/scala/akka/persistence/hazelcast/snapshot/MapSnapshotStore.scala
+++ src/main/scala/akka/persistence/hazelcast/snapshot/MapSnapshotStore.scala
@@ -1,5 +1,6 @@
 package akka.persistence.hazelcast.snapshot
 
+import akka.actor.ActorLogging
 import akka.persistence.hazelcast.HazelcastExtension
 import akka.persistence.hazelcast.util.DeleteProcessor
 import akka.persistence.serialization.{Snapshot => PersistentSnapshot}
@@ -12,7 +13,7 @@ import scala.concurrent.Future
 /**
   * @author Igor Sorokin
   */
-private[hazelcast] final class MapSnapshotStore extends SnapshotStore {
+private[hazelcast] final class MapSnapshotStore extends SnapshotStore with ActorLogging {
   import scala.collection.JavaConverters._
   import akka.persistence.hazelcast.Id.RichSnapshotMetadata
   import context.dispatcher
@@ -20,10 +21,11 @@ private[hazelcast] final class MapSnapshotStore extends SnapshotStore {
   private val snapshotMap = HazelcastExtension(context.system).snapshotMap
 
   override def loadAsync(persistenceId: String,
-                         criteria: SnapshotSelectionCriteria): Future[Option[SelectedSnapshot]] = {
+                         criteria: SnapshotSelectionCriteria): Future[Option[SelectedSnapshot]] =
     Future({
       snapshotMap.entrySet(createPredicate(persistenceId, criteria)) match {
         case snapshots if snapshots.isEmpty =>
+          log.debug(s"No snapshot is available for '$persistenceId'.")
           Option.empty
         case snapshots =>
           val entry = snapshots.asScala
@@ -31,10 +33,11 @@ private[hazelcast] final class MapSnapshotStore extends SnapshotStore {
           val id = entry.getKey
           val snapshot = entry.getValue
           val metadata = SnapshotMetadata(id.persistenceId, id.sequenceNr, snapshot.timestamp)
+          log.debug(s"Got '${snapshots.size()}' snapshots for '$persistenceId'. " +
+            s"The one with sequenceNr '${id.sequenceNr}' was chosen.")
           Option(SelectedSnapshot(metadata, snapshot.snapshot.data))
       }
     })
-  }
 
   override def saveAsync(metadata: SnapshotMetadata, snapshot: Any): Future[Unit] =
     Future(snapshotMap.set(metadata.toId, Snapshot(metadata, PersistentSnapshot(snapshot))))
@@ -42,7 +45,10 @@ private[hazelcast] final class MapSnapshotStore extends SnapshotStore {
   override def deleteAsync(metadata: SnapshotMetadata): Future[Unit] = Future(snapshotMap.delete(metadata.toId))
 
   override def deleteAsync(persistenceId: String, criteria: SnapshotSelectionCriteria): Future[Unit] =
-    Future(snapshotMap.executeOnEntries(DeleteProcessor, createPredicate(persistenceId, criteria)))
+    Future({
+      val entries = snapshotMap.executeOnEntries(DeleteProcessor, createPredicate(persistenceId, criteria))
+      log.debug(s"'${entries.size()}' snapshots for '$persistenceId' has been deleted.")
+    })
 
   private def createPredicate(persistenceId: String, criteria: SnapshotSelectionCriteria): Predicate[_, _] = {
     val predicateBuilder = new PredicateBuilder()
